7fe1a07ae16f64b1bdfab9bd6896d7f9c1bfbea0
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-08-01 06:39:39 +0800

    added linker optimization

--- include/common.h
+++ include/common.h
@@ -1,6 +1,7 @@
+#include <stdalign.h>
 #include <string.h>
 #ifndef COMMON_H
-#define COMMON_H
+#define COMM
 
 // idk why but i just feel like i need to make this without a single conditional
 // statement
@@ -20,7 +21,8 @@
 #define MAX_CONNECTIONS 128
 #define BUF_SIZE 10000
 #define MAX_PATH_LENGTH 512
-#define MAX_HEADER_SIZE 2048
+#define MAX_RECV_SIZE 1024
+#define MAX_HEADER_SEND 64
 #define LOG_DIR "./log/server.log"
 
 // HTTP response templates and status codes
@@ -29,26 +31,41 @@ typedef enum {
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
   BAD_REQUEST,
-  RESPONSE_ERR
+  RESPONSE_COUNT,
+  RESPONSE_UNKNOWN = -1
 } ResponseType;
+
+typedef struct {
+  char text[MAX_HEADER_SEND];
+} HeaderTemplate_t;
 // Content
 typedef enum {
   CONTENT_TYPE_HTML,
   CONTENT_TYPE_TEXT,
   CONTENT_TYPE_JSON,
-  CONTENT_COUNT
+  CONTENT_COUNT,
+  CONTENT_UNKNOWN = -1
 } ContentType;
+
+typedef struct {
+  char text[MAX_HEADER_SEND];
+} ContentTemplate_t;
 
 // Connection header
 typedef enum {
   CONNECTION_CLOSE,
   CONNECTION_KEEP_ALIVE,
-  CONNECTION_COUNT
+  CONNECTION_COUNT,
+  CONNECTION_UNKNOWN = -1
 } ConnectionType;
 
-extern const char *const CONTENT[CONTENT_COUNT];
-extern const char *const RESPONSE_TEMPLATE[RESPONSE_ERR];
-extern const char *const CONNECTION_HEADER[CONNECTION_COUNT];
+typedef struct {
+  char text[MAX_HEADER_SEND];
+} ConnectionTemplate_t;
+
+extern const ContentTemplate_t CONTENT[CONTENT_COUNT];
+extern const HeaderTemplate_t RESPONSE_HEADER_TYPE[RESPONSE_COUNT];
+extern const ConnectionTemplate_t CONNECTION_TEMPLATE[CONNECTION_COUNT];
 
 // Term text color
 #define COLOR_RED "\x1b[31m"
