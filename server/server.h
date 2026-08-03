614b22a8e45dc1274195e2b09ba2b271367bc2e4
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-08-03 11:28:18 +0800

    added send all function

--- server/server.h
+++ server/server.h
@@ -8,12 +8,19 @@
 #include <netdb.h>
 
 typedef struct ListenerLoopContext ListenerLoopContext;
+typedef struct SendAllLoopContext SendAllLoopContext;
 typedef void (*ListenerStateAction)(ListenerLoopContext *);
+typedef void (*SendAllStateAction)(int *);
 struct ListenerLoopContext {
-  int fd;
-  int state;
   struct addrinfo *info;
   ListenerStateAction *actions;
+  int fd;
+  int state;
+};
+struct SendAllLoopContext {
+  SendAllStateAction *actions;
+  int state;
+  int returnValue;
 };
 
 nerrh_ft *gai_handler[2];
