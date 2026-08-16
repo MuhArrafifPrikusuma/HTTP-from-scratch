d345e4fbd7cb36ac00f4503314de9f2ba28d9c6a
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-08-17 03:51:44 +0800

    New[Parser] added getContent this function will drop the fields and take only the content of that fields

--- include/common.c
+++ include/common.c
@@ -1,19 +1,19 @@
 #include "common.h"
 
-alignas(64) const char *const CONTENT[CONTENT_COUNT] = {
+const char *const CONTENT[CONTENT_COUNT] = {
     [CONTENT_TYPE_HTML] = "Content-Type: text/html; charset=UTF-8\r\n",
     [CONTENT_TYPE_TEXT] = "Content-Type: text/plain; charset=UTF-8\r\n",
     [CONTENT_TYPE_JSON] = "Content-Type: application/json\r\n",
 };
 
-alignas(64) const char *const RESPONSE_TYPE[RESPONSE_COUNT] = {
+const char *const RESPONSE_TYPE[RESPONSE_COUNT] = {
     [OK] = "HTTP/1.1 200 OK\r\n",
     [NOT_FOUND] = "HTTP/1.1 404 Not Found\r\n",
     [INTERNAL_SERVER_ERROR] = "HTTP/1.1 500 Internal Server Error\r\n",
     [BAD_REQUEST] = "HTTP/1.1 400 Bad Request\r\n",
 };
 
-alignas(64) const char *const CONNECTION_HEADER[CONNECTION_COUNT] = {
+const char *const CONNECTION_HEADER[CONNECTION_COUNT] = {
     [CONNECTION_CLOSE] = "Connection: close\r\n",
     [CONNECTION_KEEP_ALIVE] = "Connection: keep-alive\r\n",
 };
